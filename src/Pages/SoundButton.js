1230e1bb85319554cacf307f5fc9fb154a936108
Mohd Azeem <98377116+Aziim1284@users.noreply.github.com>
2024-09-24 17:52:28 +0530

    Update SoundButton.js

--- src/Pages/SoundButton.js
+++ src/Pages/SoundButton.js
@@ -1,29 +1,3 @@
-// import React from 'react';
-// import useSoundBoard from '../Components/useSoundBoard';
-
-// const SoundButton = ({ soundUrl, label, id, isPlaying, setCurrentlyPlaying }) => {
-//     const { playSound, pauseSound } = useSoundBoard(soundUrl);
-
-//   const handlePlaySound = () => {
-//     setCurrentlyPlaying(label);
-//     playSound();
-//   };
-
-//   return (
-//     <div >
-//     <button
-//       id={id}
-//       onClick={handlePlaySound}
-//       className={`sound-button ${isPlaying ? 'playing' : ''}`}
-//     >
-//       {label}
-//     </button>
-//   </div>
-    
-//   );
-// };
-
-// export default SoundButton;
 import React from 'react';
 import useSoundBoard from '../Components/useSoundBoard';
 
